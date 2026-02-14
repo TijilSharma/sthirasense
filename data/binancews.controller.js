dd3e92e61000d5d1893032161cdbf677f3bd2e25
Tijil Sharma <sharmatijil06@gmail.com>
2026-02-14 23:59:04 +0530

    changed stream url

--- data/binancews.controller.js
+++ data/binancews.controller.js
@@ -2,7 +2,7 @@ import { prepData } from "./routes/data.controller.js";
 import WebSocket from 'ws';
 
 function createWS(coin, stream) {
-  const url = `wss://stream.binance.com:9443/ws/${stream}`;
+  const url = `wss://stream.binance.us:9443/ws/${stream}`;
   const ws = new WebSocket(url);
 
   ws.on('open', () => console.log(`[${coin}] Connected to Binance WS`));
